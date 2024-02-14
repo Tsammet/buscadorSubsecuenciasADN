c55fd7e4738ea86fd5026ff27e14a144beef13d2
Tsammet <carlos61658011@gmail.com>
2024-02-14 17:22:01 -0500

    segundo commit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let cadena = document.getElementById('cadena').value;
-let subsecuencia = document.getElementById('subsecuencia').value;  
-const subcad = (cadena, subsecuencia) => {
+const datos = () =>{
+    let cadena = document.getElementById('cadena').value;
+    let subsecuencia = document.getElementById('subsecuencia').value
+    const imprimirsubcadena = subcad(cadena, subsecuencia)
+}
 
-    
+const subcad = (cadena,subsecuencia) => {
+
+
     document.getElementById("mensaje").innerHTML = "La Cadena es: " + cadena + " La subsecuencia es: " + subsecuencia
 
 }
+
