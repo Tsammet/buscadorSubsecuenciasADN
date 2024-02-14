eeb801453cbe892d65b9211c87d9e8a8ffe5e798
Tsammet <carlos61658011@gmail.com>
2024-02-14 17:10:12 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let cadena = document.getElementById('cadena').value;
+let subsecuencia = document.getElementById('subsecuencia').value;  
+const subcad = (cadena, subsecuencia) => {
+
+    
+    document.getElementById("mensaje").innerHTML = "La Cadena es: " + cadena + " La subsecuencia es: " + subsecuencia
+
+}
