0c6d76304ec88c866d5754b815efa152fe9fc3ea
Tsammet <carlos61658011@gmail.com>
2024-02-14 22:24:14 -0500

    terminandolo

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const subcad = (cadena,subsecuencia) => {
+
+    const posicion = cadena.indexOf(subsecuencia)
+    
+    if(posicion >= 0){
+        document.getElementById("mensaje").innerHTML = "La Cadena es: " + cadena + "<br> Y la secuencia que deseas buscar es: " + subsecuencia + "<br> que se encuentra en la posición : " + posicion
+    }else{
+        document.getElementById("mensaje").innerHTML = "No se encontro la subsecuencia dentro de la cadena"
+    }
+    
+}
+
 const datos = () =>{
     let cadena = document.getElementById('cadena').value;
     let subsecuencia = document.getElementById('subsecuencia').value
     const imprimirsubcadena = subcad(cadena, subsecuencia)
 }
-
-const subcad = (cadena,subsecuencia) => {
-
-
-    document.getElementById("mensaje").innerHTML = "La Cadena es: " + cadena + " La subsecuencia es: " + subsecuencia
-
-}
-
